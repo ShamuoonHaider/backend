6c08502656f978ec36ecbc32b2ccf66ae21400c0
Shamuoon Haider <shamuoonh08@gmail.com>
2025-12-30 23:52:17 +0500

    core routing concepts

--- 02-req-nd-res/app.js
+++ 02-req-nd-res/app.js
@@ -2,17 +2,21 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write(
-    '<body><h1>Hello from my Node.js Server!</h1><form method="POST" action="/"><input type="text" name="message"/><button type="submit">Send</button></form></body>'
-  );
-  res.write("</html>");
-  res.end();
+
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Home Page</h1></body>");
+    res.write("</html>");
+    return res.end();
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+  }
 });
 
-const PORT = 4000;
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
